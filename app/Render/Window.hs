2852f085617cabf326999132eea4fdb4f2ead375
FunkyCosmonaut <FunkyCosmonaut@protonmail.com>
2023-02-28 15:13:33 -0900

    GHC downgraded for compatibility with GLFW bindings

--- app/Render/Window.hs
+++ app/Render/Window.hs
@@ -8,7 +8,7 @@ myPoints = [(sin (2*pi*k/12), cos(2*pi*k/12), 0) | k <- [1..12]]
 display :: DisplayCallback
 display = do
   clear [ ColorBuffer ]
-  renderPrimitive Points $
+  renderPrimitive TriangleFan $
     mapM_ (\(x,y,z) -> vertex $ Vertex3 x y z) myPoints
   flush
 
